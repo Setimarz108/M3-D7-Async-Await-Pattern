56f4b0be62209cfbd83e4880fb312b5c8c101a25
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2021-12-07 17:04:10 +0100

    display data

--- app.js
+++ app.js
@@ -3,21 +3,56 @@
 
 const getInput = () => {
     let input = document.getElementById('exampleFormControlInput1')
- let value = input.value
-console.log(value)
+ let word = input.value
+console.log(word)
 }
 
 getInput()
 
+// let filteredObject = []
 
+//    filteredObject = userData.filter((Data) => 
+//       userData.name.toLowerCase().includes(value.toLowerCase()))
 
-   let filteredObject = () => {
-      userData.filter(userData => 
-      userData.name.toLowerCase().includes(value.toLowerCase()))
+   
+function search(event) {
 
-   }
+        const dropdown = document.getElementsByClassName("dropDown").innerText
+        console.log(dropdown)
 
+    if (event.length < 3) {
 
+        const Query = event.target.value
+        console.log(Query)
+      filteredList = userData;
+      console.log(userData)
+      return;
+    }
+
+    filteredUsers = userData.filter((user) =>
+      user.name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    console.log(filteredUsers);
+    
+  }
+
+function displayUsers(userData) {
+    const tableBody = document.querySelector(".table-body");
+    tableBody.innerHTML = ""
+        
+    console.log(userData)
+        userData.forEach((userData) => {
+          const trnew = document.createElement("tr");
+
+          trnew.innerHTML = `
+          <th scope="row">${userData.id}</th>
+          <td>${userData.name}</td>
+          <td>${userData.username}</td>
+          <td>${userData.email}</td> ` ;
+          tableBody.appendChild(trnew);
+        });
+  }
 
 
 const getData = async () => {
@@ -27,12 +62,21 @@ const getData = async () => {
 
        if (response.ok){
            
-        const userData = await response.json()
-        console.log(userData)
-        filteredObject(userData)
-       console.log(filteredObject);
+        const userDatas = await response.json()
+        console.log(userDatas)
+        
+
+         displayUsers(userDatas)
+        //  console.log(displayUsers()) error
        }
-   }
+    //    search(userData)
+    //    console.log(search(userData));
+
+        
+        
+   } 
+
+
 
    catch (err) {
 
@@ -40,4 +84,16 @@ const getData = async () => {
    }
 }
 
-getData()
+
+
+
+
+
+
+
+
+
+window.onload = async () => {
+   
+    await getData()
+  };
